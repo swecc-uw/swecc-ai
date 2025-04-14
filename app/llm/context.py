058ed01cb1bb31a8a0b5ec21bc19cc817a65b8d7
Advay Patil <advaypatil27@gmail.com>
2025-04-13 20:57:56 -0700

    Address feedback

--- app/llm/context.py
+++ app/llm/context.py
@@ -28,44 +28,39 @@ class ContextManager:
             self.context: dict[str, deque[Message]] = {}
             self.initialized = True  # Mark as initialized
 
-    def add_context_config(self, key, **kwargs):
-        for required_arg in ContextConfig.__dataclass_fields__.keys():
-            if required_arg not in kwargs:
-                raise ValueError(f"Missing required argument: {required_arg}")
-
+    def add_context_config(self, key: str, **kwargs):
         self.context_configs[key] = ContextConfig(**kwargs)
 
-    def _update_context(self, key, message: Message):
+    def _update_context(self, key: str, message: Message):
         current_length = sum(map(len, self.context[key]))
+        max_length = self.context_configs[key].max_context_length
         while (
-            len(self.context[key]) > 0
-            and current_length + len(message)
-            >= self.context_configs[key].max_context_length
+            len(self.context[key]) > 0 and current_length + len(message) >= max_length
         ):
             current_length -= len(self.context[key].popleft())
 
         self.context[key].append(message)
-        logger.info(f"Context updated: {self.context[key]}")
+        logger.info(f"Context for key `{key}` updated: {message}")
 
-    def _ensure_relevant_context(self, key):
+    def _ensure_relevant_context(self, key: str):
         # Clear context if most recent message is older than context_invalidation_time
         if (
             len(self.context[key]) > 0
             and (datetime.now() - self.context[key][-1].timestamp).total_seconds()
             > self.context_configs[key].context_invalidation_time_seconds
         ):
-            logger.info("Clearing context...")
+            logger.info(f"Clearing context for key {key}...")
             self.context[key].clear()
 
-    def add_message_to_context(self, key, message: Message):
+    def add_message_to_context(self, key: str, message: Message):
         if key not in self.context:
-            raise ValueError(f"Context key {key} not found.")
+            raise ValueError(f"Context key `{key}` not found.")
         self._update_context(key, message)
 
-    def contextualize_prompt(self, key, prompt):
+    def contextualize_prompt(self, key: str, prompt: str):
         if key not in self.context:
-            raise ValueError(f"Context key {key} not found.")
+            raise ValueError(f"Context key `{key}` not found.")
         self._ensure_relevant_context(key)
         context = self.context[key]
         context_str = "\n".join([str(msg) for msg in context])
-        return "<CONTEXT>\n" + context_str + "\n</CONTEXT>\n" + prompt
+        return f"<CONTEXT>\n{context_str}\n</CONTEXT>\n{prompt}"
--- app/llm/gemini.py
+++ app/llm/gemini.py
@@ -2,6 +2,7 @@ import os
 from google import genai
 from google.genai import types
 import logging
+from typing import Optional
 
 logger = logging.getLogger(__name__)
 
@@ -17,7 +18,7 @@ class Gemini:
     def __init__(self):
         if not hasattr(self, "initialized"):
             self.api_key = os.getenv("GEMINI_API_KEY")
-            self.model_name = "gemini-2.0-flash-001"
+            self.model_name = os.getenv("GEMINI_MODEL_NAME", "gemini-2.0-flash")
 
             if self.api_key is None:
                 raise ValueError("GEMINI_API_KEY environment variable not set")
@@ -25,7 +26,7 @@ class Gemini:
             self.client = genai.Client()
             self.initialized = True
 
-    async def prompt_model(self, prompt, system_instruction=None):
+    async def prompt_model(self, prompt: str, system_instruction: Optional[str] = None):
         config = types.GenerateContentConfig(
             system_instruction=system_instruction,
             max_output_tokens=500,
@@ -43,13 +44,13 @@ class Gemini:
         except Exception as e:
             logger.error(f"Error in prompt_model: {e}")
 
-    async def prompt_file(self, bytes, prompt):
-        response = self.client.models.generate_content(
+    async def prompt_file(self, bytes: bytes, prompt: str, mime_type: str):
+        response = await self.client.aio.models.generate_content(
             model=self.model_name,
             contents=[
                 types.Part.from_bytes(
                     data=bytes,
-                    mime_type="application/pdf",
+                    mime_type=mime_type,
                 ),
                 prompt,
             ],
